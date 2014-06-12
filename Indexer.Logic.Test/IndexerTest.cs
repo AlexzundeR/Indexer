fa7540b1e87e0342d0d3891e41d57f11fd956bc4
AlexzundeR <grandflash@mail.ru>
2014-06-12 19:11:45 +0400

    Projects created

---
+++ Indexer.Logic.Test/IndexerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Indexer.Logic.Test
+{
+    [TestFixture]
+    public class IndexerTest
+    {
+        [Test]
+        public void MainTest()
+        {
+            
+        }
+    }
+}
